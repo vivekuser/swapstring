64661558c5caa37a8e976e0ea5a6d76f44a0d3e7
Vivek Kumar <48044537+vivekuser@users.noreply.github.com>
2022-02-04 03:46:22 +0530

    Add files via upload

---
+++ Swapstring.java
@@ -0,0 +1,13 @@
+public class Sumof
+{
+	void addition(int a, int b){
+	    
+	    System.out.println(a+b);
+	    
+	}
+	public static void main(String[] args) {
+	    Sumof sum1 = new Sumof();
+	    
+	    sum1.addition(2,7);
+	}
+}
